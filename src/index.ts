74d7ab5520c3d627849ec064f8dfd0054471f08a
Tomiogunsan <tomiafolayan@gmail.com>
2022-09-01 06:41:40 -0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+function calculateBmi () : void {
+  let weight = document.querySelector<HTMLInputElement>('#weight')?.valueAsNumber
+  let height = document.querySelector<HTMLInputElement>('#height')?.valueAsNumber
+  
+   if (weight == undefined) return
+   if(height == undefined)return
 
-import confetti from 'canvas-confetti';
+  let bmi =  weight / (height * height)
+ console.log(bmi)
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+ const heading = document.getElementById('heading') as HTMLInputElement
+ heading.innerHTML= "Your BMI is : " + String(bmi.toFixed(1))
+
+  //  let bmiOutput = document.querySelector<HTMLInputElement>('#bmi-output')!
+  //  console.log(bmiOutput)
+  // bmiOutput.textContent = String(bmi.toFixed(1))
+ 
+ 
+  if(bmi <= 18.49){
+  const message = document.getElementById('message') as HTMLInputElement
+  message.innerHTML = 'You are underweight'
+ }else if (bmi >= 18.5 && bmi <= 24.9){
+  const message = document.getElementById('message') as HTMLInputElement
+  message.innerHTML = 'You have a healthy weight'
+ } else {
+  const message = document.getElementById('message') as HTMLInputElement
+  message.innerHTML = 'You are overweight'
+ }
+ 
+ 
+}
+const submitButton = document.querySelector('#submit')
+submitButton?.addEventListener('click', calculateBmi)
+
+
+
+function reload (){
+  window.location.reload()
+}
+
+const clearButton = document.querySelector('#clear')
+clearButton?.addEventListener('click', reload)
